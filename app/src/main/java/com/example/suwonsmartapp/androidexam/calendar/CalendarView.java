0a51a39bfabba060d659c2a5b2fc4d3db99ecdfb
Junsuk Oh <a811219@gmail.com>
2015-09-08 11:04:27 +0900

    달력 예제 - GridView 상속 한 CalendarView 작성

---
+++ app/src/main/java/com/example/suwonsmartapp/androidexam/calendar/CalendarView.java
@@ -0,0 +1,33 @@
+
+package com.example.suwonsmartapp.androidexam.calendar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Created by junsuk on 15. 9. 8.. 달력
+ */
+public class CalendarView extends GridView {
+    // 코드상에서 생성될 때 호출 하는 생성자
+    public CalendarView(Context context) {
+        this(context, null);
+    }
+
+    // XML 에 정의 되었을 때 호출 되는 생성자
+    public CalendarView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    // Widget 에서 호출 했을 때
+    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init();
+    }
+
+    private void init() {
+        setNumColumns(7); // 7열로 설정
+        setBackgroundResource(android.R.color.darker_gray); // 배경을 회색으로
+    }
+}
