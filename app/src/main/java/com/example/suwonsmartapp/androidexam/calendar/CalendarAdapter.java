bbf8ecebf2c600b66823e7ac1de2afa82b6b9354
Junsuk Oh <a811219@gmail.com>
2015-09-08 11:48:57 +0900

    달력 예제 - BaseAdapter를 상속받은 CalendarAdapter 작성

--- app/src/main/java/com/example/suwonsmartapp/androidexam/calendar/CalendarAdapter.java
+++ app/src/main/java/com/example/suwonsmartapp/androidexam/calendar/CalendarAdapter.java
@@ -2,9 +2,13 @@
 package com.example.suwonsmartapp.androidexam.calendar;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.suwonsmartapp.androidexam.R;
 
 import java.util.Calendar;
 import java.util.List;
@@ -37,8 +41,35 @@ public class CalendarAdapter extends BaseAdapter {
         return position;
     }
 
+    // 아이템이 화면에 보일 때 호출 됨
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder holder;
+
+        // Layout 을 완성하고 ViewHolder와 연결
+        // 이유 : findViewById를 한번만 하려고
+        if (convertView == null) {
+            holder = new ViewHolder();
+
+            // 처음 로드
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_calendar, parent);
+
+            holder.dateTextView = (TextView) convertView.findViewById(R.id.tv_date);
+
+            convertView.setTag(holder);
+        } else {
+            // 재사용
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        // Data 를 Layout 에 설정
+        Calendar calendar = mList.get(position);
+        holder.dateTextView.setText(calendar.get(Calendar.DATE));
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView dateTextView;
     }
 }
