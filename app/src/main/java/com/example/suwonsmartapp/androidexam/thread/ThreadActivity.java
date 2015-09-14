a140493986f9667a8f7cf5c49431be590f31a86d
Junsuk Oh <a811219@gmail.com>
2015-09-14 11:22:35 +0900

    AsyncTask 예제

--- app/src/main/java/com/example/suwonsmartapp/androidexam/thread/ThreadActivity.java
+++ app/src/main/java/com/example/suwonsmartapp/androidexam/thread/ThreadActivity.java
@@ -2,13 +2,16 @@
 package com.example.suwonsmartapp.androidexam.thread;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.suwonsmartapp.androidexam.R;
@@ -25,6 +28,8 @@ public class ThreadActivity extends AppCompatActivity implements View.OnClickLis
     private TextView mNumberTextView1;
     private TextView mNumberTextView2;
 
+    private ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +42,8 @@ public class ThreadActivity extends AppCompatActivity implements View.OnClickLis
         mNumberTextView1 = (TextView) findViewById(R.id.tv_number1);
         mNumberTextView2 = (TextView) findViewById(R.id.tv_number2);
 
+        mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
+
         mThread1Btn.setOnClickListener(this);
         mThread2Btn.setOnClickListener(this);
     }
@@ -47,8 +54,15 @@ public class ThreadActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.btn_thread1:
                 progressDialogExam();
 
+                // 굉장히 오래 걸리는 처리 (10초)
+                // for (...)
+
+                // 완료되었습니다.
+
                 break;
             case R.id.btn_thread2:
+
+                new DownloadTask().execute();
                 break;
 
         }
@@ -164,5 +178,62 @@ public class ThreadActivity extends AppCompatActivity implements View.OnClickLis
             }
         });
         thread.start();
+    }
+
+    private class DownloadTask extends AsyncTask<Void, Integer, Void> {
+        private AlertDialog.Builder mmBuilder;
+
+        // UI Thread
+        // doInBackground 전에 호출 됨
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mmBuilder = new AlertDialog.Builder(ThreadActivity.this);
+            mmBuilder.setMessage("다운로드가 완료 되었습니다");
+            mmBuilder.setNegativeButton("닫기", null);
+
+            mProgressBar.setProgress(0);
+        }
+
+        // Background 쓰레드
+        @Override
+        protected Void doInBackground(Void... params) {
+            // 다운로드 처리
+            for (int i = 0; i < 100; i++) {
+                // 0.2초 쉬고
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+
+                // onProgressUpdate를 호출
+                publishProgress(i + 1);
+            }
+            return null;
+        }
+
+        // UI Thread
+        // doInBackground 에서 publishProgress 로 호출하면 호출 됨
+        // 직접 호출 하지 않는 이유 : 죽으니까
+        // http://developer.android.com/intl/ko/reference/android/os/AsyncTask.html
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            mProgressBar.setProgress(values[0]);
+            mNumberTextView2.setText(values[0] + "%");
+        }
+
+        // UI Thread
+        // doInBackground 가 수행 된 후에 호출 됨
+        // doInBackground 에서 return 된 값이 파라메터로 넘어 옴
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            mmBuilder.show();
+        }
     }
 }
