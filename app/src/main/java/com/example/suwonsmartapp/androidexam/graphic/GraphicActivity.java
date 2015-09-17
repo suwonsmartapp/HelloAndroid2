20854f36824fb982192d4be157f7dad4f6237c4b
Junsuk Oh <a811219@gmail.com>
2015-09-17 14:24:56 +0900

    외부 저장소 권한 체크용 Util 클래스 작성

--- app/src/main/java/com/example/suwonsmartapp/androidexam/graphic/GraphicActivity.java
+++ app/src/main/java/com/example/suwonsmartapp/androidexam/graphic/GraphicActivity.java
@@ -1,43 +1,55 @@
 
 package com.example.suwonsmartapp.androidexam.graphic;
 
-import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.suwonsmartapp.androidexam.R;
 
 /**
  * Created by junsuk on 15. 9. 17..
  */
-public class GraphicActivity extends Activity {
+public class GraphicActivity extends AppCompatActivity {
+
+    private ShapeView mShapeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final ShapeView shapeView = new ShapeView(this);
-        setContentView(shapeView);
+        mShapeView = new ShapeView(this);
+        setContentView(mShapeView);
+    }
 
-        // shapeView.setOnClickListener(new View.OnClickListener() {
-        // @Override
-        // public void onClick(View v) {
-        // Toast.makeText(GraphicActivity.this, "클릭",
-        // Toast.LENGTH_SHORT).show();
-        //
-        // // shapeView.startDrag(null, new View.DragShadowBuilder(shapeView),
-        // null, 0);
-        // }
-        // });
-        //
-        // shapeView.setOnTouchListener(new View.OnTouchListener() {
-        // @Override
-        // public boolean onTouch(View v, MotionEvent event) {
-        // Log.d("ShapeView", "onTouchEvent");
-        // if (event.getAction() == MotionEvent.ACTION_MOVE) {
-        // shapeView.startDrag(null, new View.DragShadowBuilder(shapeView),
-        // shapeView, 0);
-        // }
-        // return true;
-        // }
-        // });
+    // 메뉴 생성 부분
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    // 메뉴를 클릭 했을 때 처리
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                save();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void save() {
+        Toast.makeText(GraphicActivity.this, "save", Toast.LENGTH_SHORT).show();
+        mShapeView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = mShapeView.getDrawingCache();
+        mShapeView.setDrawingCacheEnabled(false);
     }
 }
---
+++ app/src/main/java/com/example/suwonsmartapp/androidexam/utils/storage/StorageUtil.java
@@ -0,0 +1,23 @@
+
+package com.example.suwonsmartapp.androidexam.utils.storage;
+
+import android.os.Environment;
+
+/**
+ * Created by junsuk on 15. 9. 17..
+ */
+public class StorageUtil {
+
+    /* Checks if external storage is available for read and write */
+    public static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    /* Checks if external storage is available to at least read */
+    public static boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
+    }
+}
