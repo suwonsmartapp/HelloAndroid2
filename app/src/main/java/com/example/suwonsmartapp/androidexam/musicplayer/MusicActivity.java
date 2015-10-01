5f88cd8eaf4070f48c4679ffb37ad5dc006ee014
Junsuk Oh <a811219@gmail.com>
2015-10-01 15:11:29 +0900

    Service - Music Player - 파일 선택 구현

--- app/src/main/java/com/example/suwonsmartapp/androidexam/musicplayer/MusicActivity.java
+++ app/src/main/java/com/example/suwonsmartapp/androidexam/musicplayer/MusicActivity.java
@@ -1,18 +1,45 @@
 package com.example.suwonsmartapp.androidexam.musicplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.suwonsmartapp.androidexam.R;
 
 /**
  * Created by junsuk on 15. 10. 1..
  */
-public class MusicActivity extends AppCompatActivity {
+public class MusicActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int REQUEST_PICK_MUSIC = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_music);
+
+        findViewById(R.id.btn_file_pick).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_file_pick:
+                pickFile();
+                break;
+        }
+    }
+
+    private void pickFile() {
+        // 음악 파일을 선택 하고
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("audio/*");
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(Intent.createChooser(intent, "음악 파일 선택"), REQUEST_PICK_MUSIC);
+        }
+
+        // 정보들을 화면에 표시
     }
 }
