85db639cd87df2ba189298e2381aeceaab04f4a7
Junsuk Oh <a811219@gmail.com>
2015-10-01 16:07:35 +0900

    Service - Music Player - 미디어 정보 가져오는 객체 MediaMetaDataRetriever 사용예

--- app/src/main/java/com/example/suwonsmartapp/androidexam/musicplayer/MusicActivity.java
+++ app/src/main/java/com/example/suwonsmartapp/androidexam/musicplayer/MusicActivity.java
@@ -1,9 +1,11 @@
 package com.example.suwonsmartapp.androidexam.musicplayer;
 
 import android.content.Intent;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.suwonsmartapp.androidexam.R;
 
@@ -40,6 +42,23 @@ public class MusicActivity extends AppCompatActivity implements View.OnClickList
             startActivityForResult(Intent.createChooser(intent, "음악 파일 선택"), REQUEST_PICK_MUSIC);
         }
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
         // 정보들을 화면에 표시
+        if (resultCode == RESULT_OK && requestCode == REQUEST_PICK_MUSIC) {
+            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+            retriever.setDataSource(getApplicationContext(), data.getData());
+            String album = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+            String title = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+            String artist = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+            String duration = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+
+            Toast.makeText(MusicActivity.this, album + ", " + title + ", " + artist + ", " + duration, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
